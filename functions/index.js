11aa25a8155c59f123fb9baa024d09413d9a4bb6
Steven Dashevsky <stiven1999d@gmail.com>
2017-04-23 01:02:35 +0300

    Added notification send on post request

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ require('@google-cloud/debug-agent').start({allowExpressions: true});
 
 var functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const q = require('q')
 admin.initializeApp(functions.config().firebase);
 
 // Adds a message that welcomes new users into the chat.
@@ -16,7 +17,6 @@ exports.addUserToDatabase = functions.auth.user().onCreate(event => {
 });
 
 exports.sendWelcomNotification = functions.database.ref('Users/{UUID}/deviceToken').onWrite(event=> {
-
     var def = q.defer();
 
     var registrationToken = event.data.val();
@@ -24,22 +24,15 @@ exports.sendWelcomNotification = functions.database.ref('Users/{UUID}/deviceToke
         def.reject();
         return def.promise;
     }
-    // See the "Defining the message payload" section below for details on how to define a message payload.
-
-    getUIDByUsername('stiva1999@gmail.com')
+    getUIDByemail('stiva1999@gmail.com')
         .then((uid) => {
-
+            // See the "Defining the message payload" section below for details on how to define a message payload.
             var payload = {
                 notification: {
                     priority: "high",
                     title: 'Welcom to Floryt'
-                },
-                data: {
-                    uid: uid
                 }
             };
-
-            // console.log(registrationToken);
             admin.messaging().sendToDevice(registrationToken, payload)
                 .then(function (response) {
                     // See the MessagingDevicesResponse reference documentation for the contents of response.
@@ -50,13 +43,10 @@ exports.sendWelcomNotification = functions.database.ref('Users/{UUID}/deviceToke
                     console.log("Error sending message:", error);
                     def.reject();
                 });
-
         })
-        .catch(() => def.reject());
-
+        .catch(def.reject);
 
     return def.promise;
-
 });
 
 exports.DllCommunication = functions.https.onRequest((req, res) => {
@@ -66,75 +56,108 @@ exports.DllCommunication = functions.https.onRequest((req, res) => {
     }
     else if (req.method == 'POST') {
         console.log(req.body);
-        const username = req.body.username;
+        const email = req.body.email;
         const computerUID = req.body.computerUID;
-        var answer = {
-            access: getPromition(username, computerUID),
-            message: 'Custom massage from admin'
-        };
-        answer = JSON.stringify(answer);
+        const isGuest = req.body.isGuest;
 
-        res.status(200).send(answer);
-    }
-    else {
-        res.status(404).send('Method not supported')
-    }
-});
-
-exports.DllCommunicationDelay = functions.https.onRequest((req, res) => {
-    console.log("Got: ", req.method);
-    if (req.method == "GET") {
-        setTimeout(function () {
-            res.status(200).send('OK');
-        }, 10000);
-    }
-    else if (req.method == 'POST') {
-        console.log(req.body);
-        const username = req.body.username;
-        const computerUID = req.body.computerUID;
-        setTimeout(function () {
+        getPromition(email, computerUID).then((isApproved) =>{
             var answer = {
-                access: getPromition(username, computerUID),
-                message: 'delayed massage from admin'
+                access: isApproved,
+                message: 'Custom massage from admin'
             };
             answer = JSON.stringify(answer);
+            console.log("Sent: ", answer);
             res.status(200).send(answer);
-        }, 10000);
+        }); 
     }
     else {
         res.status(404).send('Method not supported')
     }
 });
 
-function getPromition(username, computerUID) {
-    var adminUIDs = findAdmins(computerUID);
-    var userUID = getUIDByUsername(username);
-    adminUIDs.forEach(function (adminUID) {
-        sendNotification(adminUID, username, computerUID);
-    }, this);
-    sendNotification(userUID, username, computerUID);
-    return username === 'Steven' && computerUID === "123456789" ? true : false;
-}
-
-function findAdmins(params) {
-    return new Array(0);
-}
-
-function getUIDByUsername(username) {
-
+function getPromition(email, computerUID) {
     var def = q.defer();
-
-    admin.auth()
-        .getUserByEmail(username)
-        .then(function (userRecored) {
-            console.log(userRecored.uid);
-            def.resolve(userRecored.uid)
-        });
+    
+    getUIDByemail(email).then((uid) =>{
+        return getTokenByUID(uid, email, computerUID);
+    }).then((token) =>{
+        return sendNotification(token, email, computerUID);
+    }).then((isSent)=>{
+        def.resolve(isSent);
+    }).catch((error) =>{
+        console.log("Error in getting promition: ", error);
+        def.reject();
+    });
 
     return def.promise;
 }
 
-function sendNotification(userUID, username, computerUID) {
+function findAdminsByComputer(params) {
+    var def = q.defer();
+
+    return def.promise;
+}
+
+function getUIDByemail(email) {
+    console.log("Getting UID of ", email);
+    var def = q.defer();
+    admin.auth().getUserByEmail(email)
+        .then(function (userRecored) {
+            console.log("Got UID: ", userRecored.uid);
+            def.resolve(userRecored.uid);
+        }).catch((error) =>{
+            console.log("Error getting UID: ", error);
+            def.reject();
+        });
+    return def.promise;
+}
+
+function getTokenByUID(uid) {
+    console.log("Getting token of ", uid);
+    var def = q.defer();
+    if (uid == undefined){
+        def.reject();
+        return def.promise;admin.database()
+    }
+    var db = admin.database();
+    var usersRef = db.ref('Users');
+    var userRef = usersRef.child(uid);
+    userRef.child('deviceToken')
+        .on('value', snapshot => {
+            console.log("Got token: ", snapshot.val());
+            def.resolve(snapshot.val());
+        });
+        // .catch((error) =>{
+        //     console.log("Error getting token: ", error);
+        //     def.reject();
+        // });
+    return def.promise;
+}
+
+function sendNotification(token, email, computerUID) {
+    console.log("Sending notification to: ", token);
+    var def = q.defer();
+
+    var payload = {
+        notification: {
+            priority: "high",
+            title: email + ' wants to enter your PC',
+            body: computerUID
+        }
+    };
+    console.log("Sending: ", payload);
+    admin.messaging().sendToDevice(token, payload)
+                .then(function (response) {
+                    // See the MessagingDevicesResponse reference documentation for the contents of response.
+                    console.log("Successfully sent message:", response);
+                    def.resolve(true);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                    def.reject();
+                });
+
+    return def.promise;
 }
 
 
